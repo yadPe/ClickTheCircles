b3e6ef103aad267c2319a67d404118459e53acac
YadPe <petitot.py@gmail.com>
2019-04-19 12:35:25 +0200

    sss

--- js/audio.js
+++ js/audio.js
@@ -1,9 +1,6 @@
 const audioSrc = ['https://cdn.discordapp.com/attachments/568402956991332365/568515675585380352/hitmarker_2.mp3',  'https://cdn.discordapp.com/attachments/568402956991332365/568514899177635840/erro.mp3', 'https://cdn.discordapp.com/attachments/568402956991332365/568515724096700426/wololo.mp3']
 
 const audioLib = {
-    hit: null,
-    err: null,
-    wololo: null,
 }
 
 for (let i = 0;i< audioSrc.length; i++){
